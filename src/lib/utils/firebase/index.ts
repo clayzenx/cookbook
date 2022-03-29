b28c0b1dfeeb15e2b55b2713108c0fc449ab0e9f
Vlad <clay.zenx@gmail.com>
2022-03-29 14:58:48 +0700

    Firebase getAuthInstance

--- src/lib/utils/firebase/index.ts
+++ src/lib/utils/firebase/index.ts
@@ -1,15 +1,24 @@
 import { initializeApp } from "firebase/app";
 import type firebase from "firebase/app";
-import { getAuth, inMemoryPersistence, setPersistence } from "firebase/auth";
+import type firebaseAuth from "firebase/auth";
 import { firebaseConfig } from "$lib/fb";
 
+// fb modules import
+import { getAuth } from "firebase/auth";
 
+// fb instances
 let app: firebase.FirebaseApp = null;
 
+// fb initialization
 const initApp = (config: firebase.FirebaseOptions) => {
-  if (!config) throw new Error('Firebase config is not defined')
+  if (!config) throw new Error('Firebase config is not defined');
   app = initializeApp(config);
-  if (!app) throw new Error('Firebase init app error')
+  if (!app) throw new Error('Firebase init app error');
+
+  // fb modules
+  const auth: firebaseAuth.Auth = getAuth(app);
+  if (!auth) throw new Error('Firebase init authentification error');
+
   return app
 }
 
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import { getApp } from '$utils/firebase';
+	import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
-	getApp();
+	let app = getApp();
+	let auth = getAuth();
+
+	createUserWithEmailAndPassword(auth, 'clay.zenx@gmail.com', 'E5x2L2w5')
+		.then((userCredential) => {
+			const user = userCredential.user;
+		})
+		.catch((error: { code: string; message: string }) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log(errorCode, errorMessage);
+		});
+
+	console.log(app, auth);
 </script>
 
 <slot />
