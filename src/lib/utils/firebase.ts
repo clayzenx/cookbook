cc827af9b1be626caf41361ea4703057409dee87
Vlad <clay.zenx@gmail.com>
2022-03-29 14:44:06 +0700

    Firebase typing

--- src/lib/utils/firebase.ts
+++ src/lib/utils/firebase.ts
@@ -1,7 +1,24 @@
 import { initializeApp } from "firebase/app";
+import type firebase from "firebase/app";
+import { getAuth, inMemoryPersistence, setPersistence } from "firebase/auth";
 import { firebaseConfig } from "$lib/fb";
 
-let app = null;
+
+let app: firebase.FirebaseApp = null;
+
+const initApp = (config: firebase.FirebaseOptions) => {
+  if (!config) throw new Error('Firebase config is not defined')
+  app = initializeApp(config);
+  if (!app) throw new Error('Firebase init app error')
+  return app
+}
 
 // get fb app instance
-export const getApp = () => app ? app : (app = initializeApp(firebaseConfig), app)
+export const getApp = () => {
+  try {
+    if (!app) app = initApp(firebaseConfig)
+  } catch (e) { console.error(e) }
+
+  return app
+}
+
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { getApp } from '$utils/firebase';
+
+	getApp();
 </script>
 
 <slot />
