3ef327617b926ea9cde0068de3ca6892db3c7053
Vlad <clay.zenx@gmail.com>
2022-03-30 13:19:52 +0700

    Layout template

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
-	import {
-		getApp,
-		createUserWithEmailAndPassword,
-		signInWithEmailAndPassword
-	} from '$utils/firebase';
-
+	import { goto } from '$app/navigation';
+	import { getAuth } from 'firebase/auth';
+	import { getApp } from '$utils/firebase';
 	import 'spectre.css';
 
 	getApp();
 
-	createUserWithEmailAndPassword('clay.zenx@gmail.com', 'E5x2L2w5').then(() => {
-		signInWithEmailAndPassword('zxc@yandex.ru', 'asd123zxc').then(console.log);
-	});
+	if (!getAuth().currentUser) goto('/login');
 </script>
 
-<slot />
+<div class="wrapper">
+	<div class="nav">header</div>
+	<slot />
+	<div class="footer">footer</div>
+</div>
+
+<style>
+	.wrapper {
+		width: 100vw;
+		height: 100vh;
+		box-sizing: border-box;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/login.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+</script>
+
+<div class="p-centered">
+	<h3>Login page</h3>
+</div>
