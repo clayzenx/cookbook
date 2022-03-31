b2384ed2af75131d2a6d222424666e274284bee8
Vlad <clay.zenx@gmail.com>
2022-03-31 10:06:30 +0700

    Form

---
+++ src/lib/components/loginForm.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let type = '';
+</script>
+
+<h3>{type}</h3>
+<div class="form-group">
+	<input class="form-input" type="text" id="input-example-1" placeholder="Name" />
+	<p class="form-input-hint">ads</p>
+
+	<input class="form-input" type="text" id="input-example-1" placeholder="Name" />
+	<p class="form-input-hint">ads</p>
+</div>
+<button class="btn btn-primary p-centered">Log In <i class="fi fi-rr-cloud upload" /></button>
--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
-	import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+	import LoginForm from '$components/loginForm.svelte';
+	let activeTab = 'signIn';
 </script>
 
-<div class="p-centered">
-	<h3>Login page</h3>
-	<div class="form-group">
-		<label class="form-label" for="input-example-1">Name</label>
-		<input class="form-input" type="text" id="input-example-1" placeholder="Name" />
-		<p class="form-input-hint" />
-	</div>
-	<button class="btn btn-primary p-centered">Log In</button>
+<div class="p-centered login-wrapper">
+	<ul class="tab tab-block">
+		<li
+			class="tab-item"
+			class:active={activeTab === 'signIn'}
+			on:click={() => (activeTab = 'signIn')}
+		>
+			<a>Sign In</a>
+		</li>
+		<li
+			class="tab-item"
+			class:active={activeTab === 'signUp'}
+			on:click={() => (activeTab = 'signUp')}
+		>
+			<a>Sign Up</a>
+		</li>
+	</ul>
+
+	<LoginForm type={activeTab} />
 </div>
+
+<style>
+	.login-wrapper {
+		width: 325px;
+		padding: 10px;
+	}
+	a {
+		cursor: pointer;
+	}
+</style>
