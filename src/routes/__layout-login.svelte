6b5001ddd13a2f48a535e378eb833222802d365d
Vlad <clay.zenx@gmail.com>
2022-04-12 16:22:17 +0700

    Login Layout

---
+++ src/routes/__layout-login.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Main from '$components/layout/Main.svelte';
+</script>
+
+<div class="container mx-auto">
+	<div class="flex flex-col items-center justify-center h-screen">
+		<slot />
+	</div>
+</div>
